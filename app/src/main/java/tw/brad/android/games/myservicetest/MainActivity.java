6b42a8a74673c01feca23ba4d221e7826d8108e9
Brad <brad@brad.tw>
2017-08-16 21:21:33 +0800

    v5

--- app/src/main/java/tw/brad/android/games/myservicetest/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/myservicetest/MainActivity.java
@@ -1,28 +1,109 @@
 package tw.brad.android.games.myservicetest;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private MyServiceConnection myServiceConnection;
+    private SeekBar seekBar;
+    private MyReceiver myReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myReceiver = new MyReceiver();
+        seekBar = (SeekBar)findViewById(R.id.seekbar);
+
+        IntentFilter filter = new IntentFilter("brad");
+        registerReceiver(myReceiver, filter);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    Intent it = new Intent(MainActivity.this, MyService.class);
+                    it.putExtra("progress", progress);
+                    startService(it);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+//        Intent it = new Intent(MainActivity.this, MyService.class);
+//        myServiceConnection = new MyServiceConnection();
+//        bindService(it,myServiceConnection, Context.BIND_AUTO_CREATE);
+
+    }
+
+    @Override
+    public void finish() {
+        unregisterReceiver(myReceiver);
+        super.finish();
+    }
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len", -1);
+            int now = intent.getIntExtra("now", -1);
+            if (len >= 0 ){
+                seekBar.setMax(len);
+            }
+            if (now >= 0){
+                seekBar.setProgress(now);
+            }
+        }
+    }
+
+
+    private class MyServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
     }
 
     public void start(View view){
         Intent it = new Intent(MainActivity.this, MyService.class);
+        it.putExtra("isStart", true);
         startService(it);
     }
     public void pause(View view){
         Intent it = new Intent(MainActivity.this, MyService.class);
+        it.putExtra("isPause", true);
         startService(it);
     }
     public void stop(View view){
         Intent it = new Intent(MainActivity.this, MyService.class);
         stopService(it);
     }
+    public void test(View view){
+        unbindService(myServiceConnection);
+    }
+
+
 }
--- app/src/main/java/tw/brad/android/games/myservicetest/MyService.java
+++ app/src/main/java/tw/brad/android/games/myservicetest/MyService.java
@@ -2,12 +2,21 @@ package tw.brad.android.games.myservicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyService extends Service {
+    private MediaPlayer player;
+    private Timer timer;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -18,12 +27,47 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i("brad", "onCreate");
+        timer = new Timer();
+        player = MediaPlayer.create(this,R.raw.try_everything);
+
+        int len = player.getDuration();
+        Intent it = new Intent("brad"); // action = "brad"
+        it.putExtra("len", len);
+        sendBroadcast(it);
+
+        player.start();
+        timer.schedule(new PlayTask(), 0, 200);
+
+    }
+
+    private class PlayTask extends TimerTask {
+        @Override
+        public void run() {
+            if (player != null && player.isPlaying()){
+                int now = player.getCurrentPosition();
+                Intent it = new Intent("brad"); // action = "brad"
+                it.putExtra("now", now);
+                sendBroadcast(it);
+            }
+        }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("brad", "onStartCommand");
+        boolean isStart = intent.getBooleanExtra("isStart", false);
+        boolean isPause = intent.getBooleanExtra("isPause", false);
+        int progress = intent.getIntExtra("progress", -1);
+
+        if (progress>=0) {
+            player.seekTo(progress);
+        }else if (isPause) {
+            if (player != null && player.isPlaying()){
+                player.pause();
+            }else{
+                player.start();
+            }
+        }
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -36,7 +80,14 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.i("brad", "onDestroy");
+        if (timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+        if (player != null ){
+            player.stop();
+        }
     }
 
 }
