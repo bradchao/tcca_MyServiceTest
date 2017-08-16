1ddb078e9eeb97fe6caf5405c082b304bf92d244
Brad <brad@brad.tw>
2017-08-16 19:25:17 +0800

    v3

--- app/src/main/java/tw/brad/android/games/myservicetest/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/myservicetest/MainActivity.java
@@ -1,7 +1,9 @@
 package tw.brad.android.games.myservicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view){
+        Intent it = new Intent(MainActivity.this, MyService.class);
+        startService(it);
+    }
+    public void pause(View view){
+        Intent it = new Intent(MainActivity.this, MyService.class);
+        startService(it);
+    }
+    public void stop(View view){
+        Intent it = new Intent(MainActivity.this, MyService.class);
+        stopService(it);
     }
 }
